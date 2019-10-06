80963aac4f04a211a0377fadd7bb1501fcacb4fc
Mortespj <mortespj@stud.ntnu.no>
2019-10-06 15:51:54 +0200

    Health system, player attack, triggered traps with damage and animation

---
+++ Dungeon defense/Assets/Scripts/TrapTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTrigger : MonoBehaviour
+{
+    public float damage = 50f;
+    void OnTriggerEnter(Collider other)
+    {
+      GetComponent<Animation>().Play(); //Should play the animation
+        Target target = other.transform.GetComponent<Target>();
+        if (target)
+        {
+            target.TakeDamage(damage);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GetComponent<Animation>().Stop(); //Should play the animation
+    }
+}
